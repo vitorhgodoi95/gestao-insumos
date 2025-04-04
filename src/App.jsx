458a826eabd3e981c512109164b3d34e3d1954c3
vitorhgodoi95 <vitorhgodoi.agro@gmail.com>
2025-04-04 09:24:02 -0300

    fix: ajustes finais para deploy no Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import Estoque from "./pages/Estoque";
 import ListaPreco from "./pages/Listapreco";
